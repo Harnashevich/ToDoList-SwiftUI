2d2203704dd835212721bcfb8745dd68bbc342f1
Andrei <garnash1313@gmail.com>
2024-12-16 12:53:59 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Andrei Harnashevich on 16.12.24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
